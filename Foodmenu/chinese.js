28025148d179ff996197fdc3b04415f148e71371
Madhusriram2000 <madhusriram2000@gmail.com>
2022-11-15 20:29:18 +0530

    first commit

---
+++ Foodmenu/chinese.js
@@ -0,0 +1,54 @@
+const button = document.querySelector("#increaser");
+const button1 = document.querySelector("#decreaser");
+const res = document.querySelector("#result");
+button.addEventListener("click", () => {
+  res.textContent++
+});
+button1.addEventListener("click", () => {
+    res.textContent--
+ });
+const button2=document.querySelector("#increaser1");
+const button3=document.querySelector("#decreaser1");
+const res1=document.querySelector("#result1");
+button2.addEventListener("click",()=>{
+    res1.textContent++
+});
+button3.addEventListener("click",()=>{
+  res1.textContent--
+});
+const button4=document.querySelector("#increaser2");
+const button5=document.querySelector("#decreaser2");
+const res2=document.querySelector("#result2");
+button4.addEventListener("click",()=>{
+    res2.textContent++
+});
+button5.addEventListener("click",()=>{
+  res2.textContent--
+});
+const button6=document.querySelector("#increaser3");
+const button7=document.querySelector("#decreaser3");
+const res3=document.querySelector("#result3");
+button6.addEventListener("click",()=>{
+    res3.textContent++
+});
+button7.addEventListener("click",()=>{
+  res3.textContent--
+});
+const button8=document.querySelector("#increaser4");
+const button9=document.querySelector("#decreaser4");
+const res4=document.querySelector("#result4");
+button8.addEventListener("click",()=>{
+    res4.textContent++
+});
+button9.addEventListener("click",()=>{
+  res4.textContent--
+});
+const button10=document.querySelector("#increaser5");
+const button11=document.querySelector("#decreaser5");
+const res5=document.querySelector("#result5");
+button10.addEventListener("click",()=>{
+    res5.textContent++
+});
+button11.addEventListener("click",()=>{
+  res5.textContent--
+});
---
+++ formassimanoj.js
@@ -0,0 +1,6 @@
+function handlesubmit(){
+    var a=document.getElementById("name").value;
+    var b=document.getElementById("pass").value;
+    alert(`this is ${a} + pass ${b}`)
+}
+handlesubmit()
